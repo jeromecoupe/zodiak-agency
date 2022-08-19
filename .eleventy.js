e32ba435d93857f0115960283bcade93c895337e
Jérôme Coupé <jercoupe@gmail.com>
2022-08-19 10:01:38 +0200

    remove 11ty dev server config

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,4 @@
 module.exports = function (eleventyConfig) {
-  // server options
-  eleventyConfig.setServerOptions({
-    port: 3000,
-    input: "dist",
-  });
-
   // override base config
   return {
     dir: {
