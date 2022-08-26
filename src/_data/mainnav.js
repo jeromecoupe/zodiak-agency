e791bc958d0df3d3e57b6225ff8477adfacf6523
Jérôme Coupé <jercoupe@gmail.com>
2022-08-26 11:21:09 +0200

    about page

---
+++ src/_data/mainnav.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    label: "Home",
+    trigger: "home",
+    url: "/",
+  },
+  {
+    label: "Case studies",
+    trigger: "cases",
+    url: "/case-studies/",
+  },
+  {
+    label: "About",
+    trigger: "about",
+    url: "/about/",
+  },
+];
