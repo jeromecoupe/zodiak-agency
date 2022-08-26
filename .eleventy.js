78c41c801fddbfde98ac004614fad19059bb8448
Jérôme Coupé <jercoupe@gmail.com>
2022-08-26 17:42:39 +0200

    add favicons

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   // copy
   eleventyConfig.addPassthroughCopy("./src/assets/img");
   eleventyConfig.addPassthroughCopy("./src/assets/fonts");
+  eleventyConfig.addPassthroughCopy({ "./src/static": "./" });
   // override base config
   return {
     dir: {
